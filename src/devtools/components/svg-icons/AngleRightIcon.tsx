c7ea04c33e371058efe8b5e8e9fdc3f497540609
ghazi <ghaziabbassi1@gmail.com>
2025-10-12 23:00:57 +0100

    add svg icons to be used in the project

---
+++ src/devtools/components/svg-icons/AngleRightIcon.tsx
@@ -0,0 +1,20 @@
+import { SVGProps } from "@/devtools/components/svg-icons/types";
+
+export default function AngleRightIcon(props: SVGProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="m8.25 4.5 7.5 7.5-7.5 7.5"
+      />
+    </svg>
+  );
+}
--- src/devtools/components/svg-icons/TriangleIcon.tsx
+++ src/devtools/components/svg-icons/TriangleIcon.tsx
@@ -6,26 +6,31 @@ import styles from "./TriangleIcon.module.css";
 
 export default function TriangleIcon(props: TriangleIconProps) {
   const [extra, rest] = splitProps(props, ["class"]);
-  const orientation = () => props.orientation ?? "down";
+  const orientationCssClass = () => {
+    if (props.orientation === "down") {
+      return styles.down;
+    } else if (props.orientation === "left") {
+      return styles.left;
+    } else if (props.orientation === "up") {
+      return styles.up;
+    } else {
+      return "";
+    }
+  };
 
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 286 252"
-      fill="currentCOlor"
-      stroke="currentColor"
-      class={`${styles.triangle} ${orientation() === "up" ? styles.up : ""} ${extra.class ?? ""}`}
+      viewBox="0 0 16 16"
+      fill="currentColor"
+      class={`${styles.triangle} ${orientationCssClass()} ${extra.class ?? ""}`}
       {...rest}
     >
-      <path
-        d="M186.478 208.19C165.942 239.49 120.058 239.49 99.5225 208.19L28.8848 100.525C6.19707 65.9447 31.0044 20 72.3633 20L213.637 20C254.996 20 279.803 65.9447 257.115 100.525L186.478 208.19Z"
-        stroke-width="30"
-        stroke-linejoin="round"
-      />
+      <path d="M6 12.796V3.204L11.481 8zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753" />
     </svg>
   );
 }
 
 interface TriangleIconProps extends SVGProps {
-  orientation?: "up" | "down";
+  orientation?: "up" | "down" | "left" | "right";
 }
