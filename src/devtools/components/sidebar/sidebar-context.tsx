93d1a1791f287a6551feffdb501fcddc30c42c49
ghazi <ghaziabbassi1@gmail.com>
2025-10-13 21:11:38 +0100

    update the sidebar context to include the list of databases displayed in the sidebar

--- src/devtools/components/sidebar/sidebar-context.tsx
+++ src/devtools/components/sidebar/sidebar-context.tsx
@@ -1,4 +1,4 @@
-import { createContext, FlowProps, useContext } from "solid-js";
+import { batch, createContext, FlowProps, useContext } from "solid-js";
 import { createStore } from "solid-js/store";
 
 const SidebarContext = createContext<SidebarContextType>();
@@ -13,37 +13,97 @@ export function useSidebarContext() {
 }
 
 export function SidebarContextProvider(props: FlowProps) {
-  const [store, setStore] = createStore({ isOpen: getSidebarState() });
+  const state = getSidebarState();
+  const [store, setStore] = createStore({
+    isOpen: state.isOpen,
+    databases: state.databases,
+    get selectedDatabase() {
+      return this.databases.find((db) => db.isSelected);
+    },
+  });
   const openSidebar = () => {
-    setStore({ isOpen: true });
-    storeSidebarState(true);
+    setStore("isOpen", true);
+    storeSidebarState(store);
   };
   const closeSidebar = () => {
-    setStore({ isOpen: false });
-    storeSidebarState(false);
+    setStore("isOpen", false);
+    storeSidebarState(store);
+  };
+
+  const toggleSelectedDatabase = (dbName: string) => {
+    if (store.selectedDatabase?.name === dbName) {
+      store.selectedDatabase.isSelected = false;
+      return;
+    }
+
+    batch(() => {
+      for (let i = 0; i < store.databases.length; i++) {
+        const isSelected = store.databases[i].name === dbName;
+        setStore("databases", i, "isSelected", isSelected);
+      }
+    });
   };
 
   return (
     <SidebarContext.Provider
-      value={{ sidebar: store, openSidebar, closeSidebar }}
+      value={{
+        sidebar: store,
+        openSidebar,
+        closeSidebar,
+        toggleSelectedDatabase,
+      }}
     >
       {props.children}
     </SidebarContext.Provider>
   );
 }
 
-function storeSidebarState(isOpen: boolean) {
-  localStorage.setItem("sidebar-state", isOpen ? "open" : "closed");
+function storeSidebarState(state: SidebarStore) {
+  const toStore = { isOpen: state.isOpen, databases: state.databases };
+  localStorage.setItem("sidebar-state", JSON.stringify(toStore));
 }
 
-function getSidebarState() {
+function getSidebarState(): SidebarState {
+  const defaultState = { isOpen: true, databases: [] as Database[] };
   const state = localStorage.getItem("sidebar-state");
-  // default to an open sidebar
-  return state !== "closed";
+  if (!state) return defaultState;
+
+  try {
+    const sidebarState = JSON.parse(state);
+    return { ...defaultState, ...sidebarState };
+  } catch (e) {
+    console.error(
+      "sidebar-state-retrieval: failure to get sidebar state from local storage",
+      e,
+    );
+    return defaultState;
+  }
 }
 
 interface SidebarContextType {
-  sidebar: { isOpen: boolean };
+  sidebar: SidebarStore;
   openSidebar: () => void;
   closeSidebar: () => void;
+  toggleSelectedDatabase: (dbName: string) => void;
+}
+
+interface SidebarState {
+  isOpen: boolean;
+  databases: Database[];
+}
+
+interface SidebarStore extends SidebarState {
+  readonly selectedDatabase: Database | undefined;
+}
+
+interface Database {
+  name: string;
+  version: number;
+  isSelected: boolean;
+  objectStores: ObjectStore[];
+}
+
+interface ObjectStore {
+  name: string;
+  isSelected: boolean;
 }
