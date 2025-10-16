622c81d12f9e47a8269aac40eac85bb4be2198e2
ghazi <ghaziabbassi1@gmail.com>
2025-10-16 07:34:10 +0100

    simplify showing UnstyledButton as loading

--- src/devtools/components/buttons/UnstyledButton.tsx
+++ src/devtools/components/buttons/UnstyledButton.tsx
@@ -1,16 +1,26 @@
-import { JSX, splitProps } from "solid-js";
+import { JSX, Show, splitProps } from "solid-js";
+
+import LoadingIcon from "@/devtools/components/svg-icons/LoadingIcon";
 
 import styles from "./UnstyledButton.module.css";
 
 export default function UnstyledButton(props: ButtonProps) {
-  const [extra, btnProps] = splitProps(props, ["class"]);
+  const [local, btnProps] = splitProps(props, ["class", "loading", "children"]);
   return (
     <button
       type="button"
-      class={`${styles.unstyled} ${extra.class ?? ""}`}
+      class={`${styles.unstyled} ${local.class ?? ""}`}
+      disabled={local.loading}
       {...btnProps}
-    />
+    >
+      {local.children}
+      <Show when={local.loading}>
+        <LoadingIcon class={styles.loading} />
+      </Show>
+    </button>
   );
 }
 
-type ButtonProps = JSX.ButtonHTMLAttributes<HTMLButtonElement>;
+interface ButtonProps extends JSX.ButtonHTMLAttributes<HTMLButtonElement> {
+  loading?: boolean;
+}
