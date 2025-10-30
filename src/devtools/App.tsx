7428a86430249bacb7389693ae85337146123b88
ghazi <ghaziabbassi1@gmail.com>
2025-10-30 20:53:00 +0100

    remove unused code

--- src/devtools/App.tsx
+++ src/devtools/App.tsx
@@ -1,5 +1,3 @@
-import { Toaster, ToastProvider } from "solid-notifications";
-
 import { ActiveObjectStoreContextProvider } from "@/devtools/components/active-object-store-context";
 import { IndexedDBContextProvider } from "@/devtools/components/indexeddb-context";
 import MainContent from "@/devtools/components/main-content/MainContent";
@@ -11,27 +9,18 @@ import styles from "./App.module.css";
 
 function App() {
   return (
-    <ToastProvider
-      limit={1}
-      positionX="right"
-      positionY="top"
-      showProgressBar={false}
-      dismissButtonStyle={{ "box-shadow": "none" }}
-    >
-      <Toaster />
-      <div class={styles.app}>
-        <OriginContextProvider>
-          <IndexedDBContextProvider>
-            <ActiveObjectStoreContextProvider>
-              <SidebarContextProvider>
-                <Sidebar />
-                <MainContent />
-              </SidebarContextProvider>
-            </ActiveObjectStoreContextProvider>
-          </IndexedDBContextProvider>
-        </OriginContextProvider>
-      </div>
-    </ToastProvider>
+    <div class={styles.app}>
+      <OriginContextProvider>
+        <IndexedDBContextProvider>
+          <ActiveObjectStoreContextProvider>
+            <SidebarContextProvider>
+              <Sidebar />
+              <MainContent />
+            </SidebarContextProvider>
+          </ActiveObjectStoreContextProvider>
+        </IndexedDBContextProvider>
+      </OriginContextProvider>
+    </div>
   );
 }
 
