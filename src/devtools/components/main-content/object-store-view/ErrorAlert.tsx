fc8269fa66547c87eb605be8a80fb691b75cd079
ghazi <ghaziabbassi1@gmail.com>
2025-11-10 07:40:58 +0100

    create a separate error alert component

---
+++ src/devtools/components/main-content/object-store-view/ErrorAlert.tsx
@@ -0,0 +1,42 @@
+import { For, JSX, Show } from "solid-js";
+
+import UnstyledButton from "@/devtools/components/buttons/UnstyledButton";
+import CloseIcon from "@/devtools/components/svg-icons/CloseIcon";
+
+import styles from "./ErrorAlert.module.css";
+
+export default function ErrorAlert(props: ErrorAlertProps) {
+  const errors = () => {
+    return typeof props.errorMsg === "string"
+      ? [props.errorMsg]
+      : props.errorMsg;
+  };
+
+  return (
+    <div class={`${styles.alert} ${props.useMonoFont && styles.mono}`}>
+      <Show
+        when={errors().length !== 1}
+        fallback={<div innerText={errors()[0]} />}
+      >
+        <ul>
+          <For each={errors()}>{(error) => <li innerText={error} />}</For>
+        </ul>
+      </Show>
+
+      <UnstyledButton
+        class={styles.close}
+        onClick={(event) => {
+          props.onClick?.(event);
+        }}
+      >
+        <CloseIcon />
+      </UnstyledButton>
+    </div>
+  );
+}
+
+interface ErrorAlertProps {
+  errorMsg: string | string[];
+  onClick?: JSX.EventHandler<HTMLButtonElement, MouseEvent>;
+  useMonoFont?: boolean;
+}
--- src/devtools/components/main-content/object-store-view/TableError.tsx
+++ src/devtools/components/main-content/object-store-view/TableError.tsx
@@ -1,27 +1,21 @@
 import { Show } from "solid-js";
 
-import UnstyledButton from "@/devtools/components/buttons/UnstyledButton";
+import ErrorAlert from "@/devtools/components/main-content/object-store-view/ErrorAlert";
 import { useTableMutationContext } from "@/devtools/components/main-content/object-store-view/table-mutation-context";
-import CloseIcon from "@/devtools/components/svg-icons/CloseIcon";
-
-import styles from "./TableError.module.css";
 
 export default function TableError() {
   const { tableMutationStore, setErrorMsg } = useTableMutationContext();
 
   return (
     <Show when={tableMutationStore.errorMsg}>
-      <div class={styles.alert}>
-        <div>{tableMutationStore.errorMsg}</div>
-        <UnstyledButton
-          class={styles.close}
+      {(msg) => (
+        <ErrorAlert
+          errorMsg={msg()}
           onClick={() => {
             setErrorMsg(null);
           }}
-        >
-          <CloseIcon />
-        </UnstyledButton>
-      </div>
+        />
+      )}
     </Show>
   );
 }
