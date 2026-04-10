785383f99038d9d08ac16ceffbb8e2dba86cdb63
ghazi <ghaziabbassi1@gmail.com>
2026-04-10 13:49:50 +0100

    remove the singleLineEllipsis directive since `use:` directives are removed in solid 2.0

--- src/devtools/components/SingleLineText.tsx
+++ src/devtools/components/SingleLineText.tsx
@@ -1,11 +1,33 @@
-import { singleLineEllipsis } from "@/devtools/directives/ellipsis";
+import { onMount } from "solid-js";
 
+import styles from "./SingleLineText.module.css";
+/**
+ * Adds an ellipsis when the text is too long to be shown on a single line
+ * and shows the full text in a native browser tooltip.
+ * The user of the component has to make sure the element has a defined
+ * width/max-width or use flexbox to help.
+ * https://leonardofaria.net/2020/07/18/using-flexbox-and-text-ellipsis-together
+ */
 export default function SingleLineText(props: SingleLineTextProps) {
+  let ref: HTMLDivElement;
+  onMount(() => {
+    const observer = new ResizeObserver(() => {
+      if (ref.scrollWidth > ref.clientWidth) {
+        ref.setAttribute("title", props.text);
+      } else {
+        ref.removeAttribute("title");
+      }
+    });
+    observer.observe(ref);
+  });
+
   return (
     <div
-      class={props.class ?? ""}
+      ref={(elt) => {
+        ref = elt;
+      }}
+      class={`${styles.ellipsis} ${props.class ?? ""}`}
       dir="auto"
-      use:singleLineEllipsis={props.text}
     >
       {props.text}
     </div>
